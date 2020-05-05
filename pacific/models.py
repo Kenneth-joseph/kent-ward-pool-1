bc5fde4f42ff1511fb022d4b2cde500f8a6ae1e3
Kenneth-joseph <josephochieng812@gmail.com>
2020-05-05 11:28:05 +0300

    enhanced my models

--- pacific/models.py
+++ pacific/models.py
@@ -8,12 +8,17 @@ class Voter(models.Model):
     national_id = models.IntegerField(default=0)
     phone_number = models.IntegerField(default=0)
     ward = models.CharField(max_length= 30)
+    polling_station= models.CharField(max_length=30, default='gwasi')
 
 
     def __str__(self):
-        return self.national_id
+        return self.first_name
 
 class Event(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+        
--- pool/settings.py
+++ pool/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pacific.apps.PacificConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

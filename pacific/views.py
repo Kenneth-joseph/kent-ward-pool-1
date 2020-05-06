26c9e1cd14a73300beccf80c175e5301697c99be
Kenneth-joseph <josephochieng812@gmail.com>
2020-05-06 23:05:19 +0300

    made the changes on my event section thus the home page

--- pacific/views.py
+++ pacific/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from django.http import HttpResponse
+from .models import Event,Voter
 
 
 # Create your views here.
 # def home(request):
 #     return render(request, 'home')
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model= Event
     template_name = 'home.html'
+    ordering = ['-pub_date']
 

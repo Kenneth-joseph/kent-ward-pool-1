86e7f1c31ded652717d76e74a7b2e73c4d40bb1c
Kenneth-joseph <josephochieng812@gmail.com>
2020-05-08 14:41:38 +0300

    imported reverse and used it for the event creation

--- pacific/models.py
+++ pacific/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -22,6 +23,9 @@ class Event(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('home')
 
     def __str__(self):
         return self.title

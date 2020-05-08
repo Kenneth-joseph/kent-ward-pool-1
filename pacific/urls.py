3ac1d1dd45c067c9a460c54edc0baf751ee5855c
Kenneth-joseph <josephochieng812@gmail.com>
2020-05-08 13:31:58 +0300

    Added the footer template

--- pacific/urls.py
+++ pacific/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import (HomePageView,
+                    CreateEvents,
+                    CreateVoter)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('newevent/', CreateEvents.as_view(), name='newevent'),
+    path('newvoter/', CreateVoter.as_view(), name='newvoter'),
 ]
--- pacific/views.py
+++ pacific/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView,
+    DetailView)
 from django.http import HttpResponse
-from .models import Event,Voter
+from .models import Event, Voter
 
 
 # Create your views here.
 # def home(request):
 #     return render(request, 'home')
 class HomePageView(ListView):
-    model= Event
+    model = Event
     template_name = 'home.html'
     ordering = ['-pub_date']
+    context_object_name = 'events'
 
+
+class CreateEvents(CreateView):
+    model = Event
+    template_name = 'home.html'
+    fields = ['title', 'content']
+
+
+class CreateVoter(CreateView):
+    model = Voter
+    template_name = 'home.html'
+    fields = '__all__'
+
+
+class DetailView(DetailView):
+    model = Voter
+    template_name = "home.html"
